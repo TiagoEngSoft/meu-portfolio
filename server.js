45257a84ddd8ee21db37e9973489c3aae75ff9f7
Tiago Santos <tiagoengsoftdev@gmail.com>
2024-05-19 19:47:45 -0300

    config heroku

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const experss = require('express')
-const { resolve } = require("path")
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const app = experss()
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', experss.static(resolve(__dirname, './dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-app.listen(process.env.PORT || 3000, (err) => {
-    if(err){ return console.log(err)
-    }
-
-    console.log("Tudo Funcionando corretamente!")
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
