41ac7cd6f3d5abec1047c6e83cf744a3122e609d
Tiago Santos <tiagoengsoftdev@gmail.com>
2024-05-20 05:54:37 -0300

    Cv pdf para dowload

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   }
 
   const handleDownload = () => {
-    const url = '../../assets/cv.docx';
+    const url = '../../../public/cv.pdf';
     const link = document.createElement('a');
     link.href = url;
     link.setAttribute('download', 'cv.docx');
