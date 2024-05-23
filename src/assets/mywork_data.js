e74c94f8993df7d64caeec0222eb3e6dc394d4eb
TiagoEngSoft <tiagoengsoftdev@gmail.com>
2024-05-23 12:21:12 -0300

    Vinculo com o projeto Get a Pet

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,18 +2,20 @@ import project1_img from '../assets/project_1.svg'
 import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import imageco from '../assets/eco.png'
+import getPet from '../assets/getPet.png'
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
+        w_name:"React Js",
         w_img:imageco,
         url: "https://proj-ecommerce-8102ef45380d.herokuapp.com/"
     },
     {
         w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_name:"ReactJs Express Mongo",
+        w_img:getPet,
+        url: "https://front-get-pet-5555d9e1cb20.herokuapp.com/"
     },
     {
         w_no:4,
