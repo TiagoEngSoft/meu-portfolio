30f055e0ac663c06b98375db29afddb54bbfe5bf
Tiago Santos <tiagoengsoftdev@gmail.com>
2024-05-27 20:24:59 -0300

    Novo projeto (Painel)

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,8 +3,15 @@ import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import imageco from '../assets/eco.png'
 import getPet from '../assets/getPet.png'
+import painel from '../assets/painel.png'
 
 const mywork_data = [
+    {
+        w_no:1,
+        w_name:"React Js",
+        w_img:imageco,
+        url: "https://meu-painel-gestao-31d130a43c13.herokuapp.com/dashboard"
+    },
     {
         w_no:1,
         w_name:"React Js",
@@ -12,13 +19,13 @@ const mywork_data = [
         url: "https://proj-ecommerce-8102ef45380d.herokuapp.com/"
     },
     {
-        w_no:3,
+        w_no:2,
         w_name:"ReactJs Express Mongo",
         w_img:getPet,
         url: "https://front-get-pet-5555d9e1cb20.herokuapp.com/"
     },
     {
-        w_no:4,
+        w_no:3,
         w_name:"Web design",
         w_img:project4_img
     },
