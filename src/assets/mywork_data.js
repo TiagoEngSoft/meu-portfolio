d84fb253e3f5ec912557f1288bf94264d0545f94
Tiago Santos <tiagoengsoftdev@gmail.com>
2024-06-26 20:42:05 -0300

    Adicionando novo projeto google glass

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,6 +4,7 @@ import project4_img from '../assets/project_4.svg'
 import imageco from '../assets/eco.png'
 import getPet from '../assets/getPet.png'
 import painel from '../assets/painel.png'
+import google from '../assets/google-glass.png'
 
 const mywork_data = [
     {
@@ -17,6 +18,12 @@ const mywork_data = [
         w_name:"React Js",
         w_img:imageco,
         url: "https://proj-ecommerce-8102ef45380d.herokuapp.com/"
+    },
+    {
+        w_no:1,
+        w_name:"React Js",
+        w_img: google,
+        url: "https://tiagoengsoft.github.io/projeto-google-glass/"
     },
     {
         w_no:2,
